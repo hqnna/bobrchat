dc22cbbd4e4e900fee1f93a7a774a583468c3728
Matthew Hrehirchuk <me@matthew-hre.com>
2026-01-07 22:38:30 -0700

    env: handle public env issue in prod

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,10 @@
+/* eslint-disable node/no-process-env */
 import { createAuthClient } from "better-auth/react";
 
 import { clientEnv } from "~/lib/env";
 
-const baseURL = clientEnv.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000";
+// Need to patch this upstream
+const baseURL = clientEnv.NEXT_PUBLIC_BETTER_AUTH_URL || process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000";
 
 export const authClient = createAuthClient({
   baseURL,
